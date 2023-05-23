c5e7544342f33ed4c357915bf2c63b43b94a912f
kylegwlawrence <kylegwlawrence@gmail.com>
2023-05-23 18:58:52 +0000

    fixed indentations in message_slack.py

--- message_slack.py
+++ message_slack.py
@@ -4,9 +4,9 @@ from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
 def send_processed_image(image_name:str, channel_id:str) -> None:
-    """Send a file to a Slack channel using file_name.type and not including the path"""
+	"""Send a file to a Slack channel using file_name.type and not including the path"""
 	# instantiate the webclient use a bot token
-    image_path = '/images/processed/{}'.format(image_name)
+	image_path = '/images/processed/{}'.format(image_name)
 	client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
 	logger = logging.getLogger(__name__)
 	try:
@@ -22,4 +22,4 @@ def send_processed_image(image_name:str, channel_id:str) -> None:
     		logger.error("Error uploading file: {}".format(e))
 
 def send_message(text:str, channel_id:str) -> None:
-    """Send a message with custom text to slack channel"""
+	"""Send a message with custom text to slack channel"""
