0eb6cf736adaeca9efa6be26baae849714db7722
Kyle Lawrence <kylegwlawrence@gmail.com>
2023-05-23 11:53:07 -0700

    add wait time to main.py before sending new processed image

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from detect_new_raw_image import compare_file_names
 from transform_image import transform_raw_image
 from message_slack import send_processed_image
+import time
 
 # determine if new images exist in raw. Returns set of raw images not processed if any. 
 new_raw_images=list(compare_file_names('images/raw','images/processed'))
@@ -8,4 +9,5 @@ new_raw_images=list(compare_file_names('images/raw','images/processed'))
 # transform image
 for image_name in new_raw_images:
 	transform_raw_image(image_name)
+	time.sleep(3) # wait for system to write processed file
 	send_processed_image(image_name, channel_id="C058V9D6PE0")
