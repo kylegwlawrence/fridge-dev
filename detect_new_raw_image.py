8136227b3d280a1e4040f2203b88945a60642e45
Kyle Lawrence <kylegwlawrence@gmail.com>
2023-05-23 12:32:44 -0700

    trying to debug main.py

--- detect_new_raw_image.py
+++ detect_new_raw_image.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 def get_file_names(directory:str) -> set:
 	"""Loop over files in the directory and return a set of file names. Ignores sub-directories. Returns name WITHOUT extension"""
@@ -9,17 +10,19 @@ def get_file_names(directory:str) -> set:
 			image_names.append(image_name)
 	return set(image_names)
 
-def compare_file_names(directory_a:str, directory_b:str) -> set:
+def compare_file_names(raw_directory:str='images/raw', processed_directory:str='images/processed') -> set:
 	"""Compares two sets of file names from two different directories (a and b) and returns a set of missing file names with extensions.
-	If there are no missing file names, returns an empty set"""
-	directory_a_names = get_file_names(directory_a)
-	directory_b_names = get_file_names(directory_b)
+	If there are no missing file names, returns a set of int 0"""
+	raw_names = get_file_names(raw_directory)
+	processed_names = get_file_names(processed_directory)
 	#compare two sets to see if equal.
-	directory_a_names_not_in_b = directory_a_names.difference(directory_b_names)
-	if len(directory_a_names_not_in_b)!=0:
-		return directory_a_names_not_in_b
-	else:
-		directory_a_names_not_in_b = None
-		return directory_a_names_not_in_b
+	raw_images_not_processed = raw_directory.difference(processed_directory)
+	print(raw_images_not_processed)
+	#if len(raw_images_not_processed)!=0:
+	return raw_images_not_processed
+	#else:
+	#	directory_a_names_not_in_b = set()
+	#	return directory_a_names_not_in_b
 	# debugging	
-	print(directory_a_names_not_in_b)
+	
+compare_file_names()
