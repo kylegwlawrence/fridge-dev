9851c901f30a5076d943bc64b13141302fc8405b
Kyle Lawrence <kylegwlawrence@gmail.com>
2023-05-23 11:44:19 -0700

    whoops transform_raw_image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from detect_new_raw_image import compare_file_names
-from transform_image import transform_image
+from transform_image import transform_raw_image
 from message_slack import send_processed_image
 
 # determine if new images exist in raw. Returns set of raw images not processed if any. 
@@ -7,5 +7,5 @@ new_raw_images=list(compare_file_names('images/raw','images/processed'))
 
 # transform image
 for image_name in new_raw_images:
-	transform_image(image_name)
+	transform_raw_image(image_name)
 	send_processed_image(image_name, channel_id="C058V9D6PE0")
