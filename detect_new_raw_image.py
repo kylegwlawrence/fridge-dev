9bcab82f57d9966f33165c478a1c7585e76c4a4c
kylegwlawrence <kylegwlawrence@gmail.com>
2023-05-23 17:17:24 +0000

    cleaned up detect_new_raw_image.py

--- detect_new_raw_image.py
+++ detect_new_raw_image.py
@@ -13,10 +13,6 @@ def compare_file_names(directory_a:str, directory_b:str) -> set:
 	directory_a_names = get_file_names(directory_a)
 	directory_b_names = get_file_names(directory_b)
 	#compare two sets to see if equal.
-	if directory_a_names==directory_b_names:
-		print("All raw images have been processed")
-	#determine which in raw are not in processed and run those through transform function
-	else:
-		directory_a_names_not_in_b = directory_a_names.difference(directory_b_names)
-		print(directory_a_names_not_in_b)
-		return directory_a_names_not_in_b
+	directory_a_names_not_in_b = directory_a_names.difference(directory_b_names)
+	print(directory_a_names_not_in_b)
+	return directory_a_names_not_in_b
