768497a100998339a49a3e104bff2ed8cadb8e3f
Kyle Lawrence <kylegwlawrence@gmail.com>
2023-05-23 12:54:25 -0700

    removed first forward slash from image_path variable in message_slack.py

--- message_slack.py
+++ message_slack.py
@@ -6,7 +6,7 @@ from slack_sdk.errors import SlackApiError
 def send_processed_image(image_name:str, channel_id:str="C058V9D6PE0") -> None:
 	"""Send a file to a Slack channel using file_name.type and not including the path"""
 	# instantiate the webclient use a bot token
-	image_path = '/images/processed/{}'.format(image_name)
+	image_path = 'images/processed/{}'.format(image_name)
 	client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
 	logger = logging.getLogger(__name__)
 	try:
