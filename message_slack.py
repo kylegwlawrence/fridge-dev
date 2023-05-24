768be059d8ab7bd7b3e021695205c7c210adefa4
Kyle Lawrence <kylelawrence@Aspens-MacBook-Air.local>
2023-05-24 15:20:26 -0700

    added prefect and dependencies to requirements.txt, fixed indentations in message_slack.py and add another exception to main.py

--- message_slack.py
+++ message_slack.py
@@ -5,21 +5,24 @@ from slack_sdk.errors import SlackApiError
 
 def send_processed_image(image_name:str, channel_id:str="C058V9D6PE0") -> None:
 	"""Take a processed image name and upload it to a given channel_id in your Slack workspace."""
-    logger = logging.getLogger(__name__)
+	logger = logging.getLogger(__name__)
 	image_path = 'images/processed/{}'.format(image_name)
     # instantiate the webclient use a bot token
-	client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
 	try:
-    		# call method to upload files to channel
-    		result = client.files_upload_v2(
-        		channel=channel_id,
-        		initial_comment="Processed photo available for {}".format(image_name),
-        		file=image_path,
-                request_file_info=False
-    		)
-    		logger.info(result)
+		client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
 	except SlackApiError as e:
-    		logger.error("Error uploading file: {}".format(e))
+		logger.error("Error creating client: {}".format(e))
+	try:
+		# call method to upload files to channel
+		result = client.files_upload_v2(
+			channel=channel_id,
+			initial_comment="Processed photo available for {}".format(image_name),
+			file=image_path,
+			request_file_info=False
+		)
+		logger.info(result)
+	except SlackApiError as e:
+		logger.error("Error uploading file: {}".format(e))
 
 def send_message(text:str, channel_id:str) -> None:
 	"""Send a message with custom text to slack channel"""
