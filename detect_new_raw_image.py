00e9ab6e7561427a05bc28392d62ec5e75cb008d
Kyle Lawrence <kylelawrence@Aspens-MacBook-Air.local>
2023-05-24 17:02:25 -0700

    move most functions into main.py and implement prefect flows and tasks

--- detect_new_raw_image.py
+++ detect_new_raw_image.py
@@ -10,14 +10,3 @@ def get_file_names(directory:str) -> set:
 			image_name=entry.name
 			image_names.append(image_name)
 	return set(image_names)
-
-def compare_file_names(raw_directory:str='images/raw', processed_directory:str='images/processed') -> list:
-	"""Get a set of raw imagenames and a set of processed image names, compare the sets, and return 
-	a list of raw image names that have not been processed. If all raw images have a match in the 
-	processed images folder, return an empty set."""
-	raw_names = get_file_names(raw_directory)
-	processed_names = get_file_names(processed_directory)
-	# compare equality between the two sets
-	raw_images_not_processed = raw_names.difference(processed_names)
-	print(list(raw_images_not_processed))
-	return list(raw_images_not_processed)
--- main.py
+++ main.py
@@ -1,15 +1,63 @@
-from detect_new_raw_image import compare_file_names
-from transform_image import transform_raw_image
-from message_slack import send_processed_image
-import time
+from detect_new_raw_image import get_file_names
+from prefect import flow, task
+import cv2
+import logging
+import os
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
-# get a list of new images in raw that have not been processed. 
-new_raw_images=compare_file_names()
+@task(name='Get list of raw images not processed')
+def compare_file_names(raw_directory:str='images/raw', processed_directory:str='images/processed') -> list:
+	"""Get a set of raw imagenames and a set of processed image names, compare the sets, and return a list of raw image names that have not been processed. If all raw images have a match in the processed images folder, return an empty set."""
+	raw_names = get_file_names(raw_directory)
+	processed_names = get_file_names(processed_directory)
+	# compare equality between the two sets
+	raw_images_not_processed = raw_names.difference(processed_names)
+	print(list(raw_images_not_processed))
+	return list(raw_images_not_processed)
 
-# transform image(s) and send the image to Slack.
-if len(new_raw_images)!=0:
-	for image_name in new_raw_images:
-		transform_raw_image(image_name)
-		send_processed_image(image_name)
-else:
-	print('No new images to send')
+@task(name='Process raw images')
+def transform_raw_image(image_name:str, width=150, height=150 ) -> None:
+    """Take an image name from the raw image folder,read it in and comvert to greyscale, resize it, and write to processed folder as a JPEG file."""
+    # possibly use cvtColor to transform from one color space to another instead of using IMREAD_GRAYSCALE
+    img = cv2.imread('images/raw/'+image_name, cv2.IMREAD_GRAYSCALE)
+    resized = cv2.resize(img, (width, height), interpolation = cv2.INTER_AREA)
+    cv2.imwrite('images/processed/'+image_name, resized, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
+    return None
+
+@task(name='Send processed photos to Slack')
+def send_processed_image(image_name:str, channel_id:str="C058V9D6PE0") -> None:
+	"""Take a processed image name and upload it to a given channel_id in your Slack workspace."""
+	logger = logging.getLogger(__name__)
+	image_path = 'images/processed/{}'.format(image_name)
+    # instantiate the webclient use a bot token
+	try:
+		client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
+	except SlackApiError as e:
+		logger.error("Error creating client: {}".format(e))
+	try:
+		# call method to upload files to channel
+		result = client.files_upload_v2(
+			channel=channel_id,
+			initial_comment="Processed photo available for {}".format(image_name),
+			file=image_path,
+			request_file_info=False
+		)
+		logger.info(result)
+	except SlackApiError as e:
+		logger.error("Error uploading file: {}".format(e))
+
+@flow(name='Determine what raw images have not been processed, process them and upload them to Slack', log_prints=True)
+def main():
+	# get a list of new images in raw that have not been processed. 
+	new_raw_images=compare_file_names()
+	# transform image(s) and send the image to Slack.
+	if len(new_raw_images)!=0:
+		for image_name in new_raw_images:
+			transform_raw_image(image_name)
+			send_processed_image(image_name)
+	else:
+		print('No new images to send')
+
+if __name__ == '__main__':
+	main()
